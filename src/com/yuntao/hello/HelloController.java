7c9a8a7abb17132394c634aac809696201ceac2e
pengyuntao <pengyuntao@xiaomi.com>
2017-04-10 11:19:01 +0800

    添加HelloSpring

---
+++ src/com/yuntao/hello/HelloController.java
@@ -0,0 +1,22 @@
+package com.yuntao.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by pengyuntao on 16/10/12.
+ */
+@Controller
+@RequestMapping(value = "/hello", method = RequestMethod.GET)
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("msg", "Spring MVC Hello World");
+        model.addAttribute("name", "yuntao");
+        return "hello";
+    }
+
+}
